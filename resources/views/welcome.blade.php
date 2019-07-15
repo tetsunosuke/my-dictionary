8f1c820bc5524d27d1d62714185e054123b6d644
ppen.019 <ppengineer.019@gmail.com>
2019-07-15 12:45:33 +0000

    add explanation to welcome view

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -2,9 +2,21 @@
 
 @section('content')
     @if (!Auth::check())
-    <div class="text-center">
+    <div class="text-center mb-4">
         <h1 style="color:#cc0066">My Dictionary</h1>
-        <p>{!! link_to_route('register', 'アカウント作成', []) !!}してオリジナルディクショナリーを作成</p>
+    </div>
+    <div class="row border mx-0 py-3" style="background-color: #d3d3d3;">
+        <aside class="col-sm-6 text-center align-middle my-auto pb-3">
+            <a class="p-0">{!! link_to_route('register', 'アカウント作成', [], ['class' => 'btn btn-dark btn-lg btn-search col-sm-6']) !!}</a>
+        </aside>
+        <aside class="col-sm-6">
+            <p class="font-weight-bold"><u>アカウントを作成するとできること</u></p>
+            <ul>
+                <li>単語カード（公開／非公開）作成・編集</li>
+                <li>他のユーザのカードを評価</li>
+                <li>単語テスト</li>
+            </ul>
+        </aside>
     </div>
     @endif
     <div class="mx-0 my-2 border">    
